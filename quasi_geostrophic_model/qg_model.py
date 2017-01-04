b2e1a91fa213e0621e85f2364b5febe6f443c6a8
Alastair Gregory <alsgregory@aol.com>
2017-01-04 12:15:54 +0000

    made a base qg class with two-level or single-level options. corrected tests

--- quasi_geostrophic_model/qg_model.py
+++ quasi_geostrophic_model/qg_model.py
@@ -4,6 +4,8 @@ from __future__ import division, absolute_import
 
 from firedrake import *
 
+from firedrake.mg.utils import get_level
+
 from mpi4py import MPI
 
 from quasi_geostrophic_model.min_dx import MinDx
@@ -11,33 +13,12 @@ from quasi_geostrophic_model.min_dx import MinDx
 import numpy as np
 
 
-class quasi_geostrophic(object):
+__all__ = ["quasi_geostrophic", "two_level_quasi_geostrophic"]
 
-    def __init__(self, dg_fs, cg_fs, variance, prescribed_forcing=False):
 
-        """ class specifying a quasi geostrophic model
+class base_class(object):
 
-            Arguments:
-
-            :arg dg_fs: The discontinuous :class:`FunctionSpace` used for Potential Vorticity
-            :type dg_fs: :class:`FunctionSpace`
-
-            :arg cg_fs: The continuous :class:`FunctionSpace` used for the Streamfunction
-            :type cg_fs: :class:`FunctionSpace`
-
-            :arg variance: Variance of random forcing
-            :type variance: int, if zero, forcing is off
-
-            Optional Arguments:
-
-            :arg prescribed_forcing: Whether or not the forcing is prescribed. Default: False
-            :type prescribed_forcing: Boolean
-
-        """
-
-        self.prescribed_forcing = prescribed_forcing
-        if isinstance(self.prescribed_forcing, bool) is False:
-            raise ValueError('The switch for prescribed_forcing must be of boolean type')
+    def __init__(self, dg_fs, cg_fs, variance):
 
         # define function spaces
         self.Vdg = dg_fs
@@ -61,9 +42,6 @@ class quasi_geostrophic(object):
 
         # set-up noise variance
         self.variance = variance
-
-        # define current time
-        self.t = 0
 
         # define functions
         self.q_ = Function(self.Vdg)  # actual q
@@ -96,13 +74,9 @@ class quasi_geostrophic(object):
         # set-up solvers
         self.setup_solver()
 
-        super(quasi_geostrophic, self).__init__()
+        super(base_class, self).__init__()
 
     def initial_condition(self, ufl_expression):
-
-        # check for initial time
-        if self.t != 0:
-            raise ValueError("can't set initial condition when time is not zero")
 
         # interpolate expression
         self.q_.interpolate(ufl_expression)
@@ -153,31 +127,90 @@ class quasi_geostrophic(object):
         self.hlhs = self.dw * self.v * dx
         self.hrhs = (dot(grad(self.v), grad(self.u)) + self.v * self.u) * dx
 
+    def __update_u(self):
+
+        if self.variance > 0:
+            self.dw.dat.data[:] = (np.sqrt(self.const_dt.dat.data) *
+                                   np.random.normal(0, np.sqrt(self.variance),
+                                                    np.shape(self.dw.dat.data)))
+            self.u_.assign(0)
+
+            solve(self.hrhs == self.hlhs, self.u_,
+                  solver_parameters={'ksp_type': 'cg'})
+
     def __update_forcing(self):
 
         if self.variance == 0:
             self.forcing.assign(0)
         else:
-
-            if self.prescribed_forcing is False:
-                self.dw.dat.data[:] = (np.sqrt(self.const_dt.dat.data) *
-                                       np.random.normal(0, np.sqrt(self.variance),
-                                                        np.shape(self.dw.dat.data)))
-                self.u_.assign(0)
-
-                solve(self.hrhs == self.hlhs, self.u_,
-                      solver_parameters={'ksp_type': 'cg'})
-
-                self.forcingProjector.project()
-
-            if self.prescribed_forcing is True:
-                # here self.u_ is already prescribed by user
-                self.forcingProjector.project()
+            self.forcingProjector.project()
 
     def __update_q_forced(self):
 
         self.__update_forcing()
         self.q_forced.assign(self.forcing + self.q_old)
+
+    def timestep(self):
+
+        # set old time-step to be current one
+        self.q_old.assign(self.q_)
+
+        # 1st RK step
+        self.psi_solver.solve()
+        self.q_solver.solve()
+        self.q_old.assign(self.dq)
+
+        # 2nd RK step
+        self.psi_solver.solve()
+        self.q_solver.solve()
+        self.q_old.assign(((3.0 / 4.0) * self.q_) + ((1.0 / 4.0) * self.dq))
+
+        # 3rd RK step
+        self.psi_solver.solve()
+        self.q_solver.solve()
+        self.q_.assign(((1.0 / 3.0) * self.q_) + ((2.0 / 3.0) * self.dq))
+
+
+class quasi_geostrophic(object):
+
+    def __init__(self, dg_fs, cg_fs, variance):
+
+        """ class specifying a randomly forced quasi geostrophic model
+
+            Arguments:
+
+            :arg dg_fs: The discontinuous :class:`FunctionSpace` used for Potential Vorticity
+            :type dg_fs: :class:`FunctionSpace`
+
+            :arg cg_fs: The continuous :class:`FunctionSpace` used for the Streamfunction
+            :type cg_fs: :class:`FunctionSpace`
+
+            :arg variance: Variance of random forcing
+            :type variance: int, if zero, forcing is off
+
+        """
+
+        self.qg_class = base_class(dg_fs, cg_fs, variance)
+
+        self.variance = self.qg_class.variance
+        self.mesh = self.qg_class.mesh
+
+        self.dt = self.qg_class.dt
+
+        self.q_ = self.qg_class.q_
+        self.psi_ = self.qg_class.psi_
+
+        self.t = 0
+
+        super(quasi_geostrophic, self).__init__()
+
+    def initial_condition(self, ufl_expression):
+
+        # check for initial time
+        if self.t != 0:
+            raise ValueError("can't set initial condition when time is not zero")
+
+        self.qg_class.initial_condition(ufl_expression)
 
     def timestepper(self, T):
 
@@ -192,30 +225,188 @@ class quasi_geostrophic(object):
 
             # re-adjust timestep if over the jump need to reach t
             if self.t + self.dt > T:
-                self.const_dt.assign(T - self.t)
+                self.qg_class.const_dt.assign(T - self.t)
             else:
-                self.const_dt.assign(self.dt)
+                self.qg_class.const_dt.assign(self.dt)
 
-            # update forcing
-            self.__update_q_forced()
-
-            # set old time-step to be current one
-            self.q_old.assign(self.q_)
-
-            # 1st RK step
-            self.psi_solver.solve()
-            self.q_solver.solve()
-            self.q_old.assign(self.dq)
-
-            # 2nd RK step
-            self.psi_solver.solve()
-            self.q_solver.solve()
-            self.q_old.assign(((3.0 / 4.0) * self.q_) + ((1.0 / 4.0) * self.dq))
-
-            # 3rd RK step
-            self.psi_solver.solve()
-            self.q_solver.solve()
-            self.q_.assign(((1.0 / 3.0) * self.q_) + ((2.0 / 3.0) * self.dq))
+            # carry out timesteps and update forcing separately
+            self.qg_class._base_class__update_u()  # if one wants to specify u, replace line
+            self.qg_class._base_class__update_q_forced()
+            self.qg_class.timestep()
 
             # update time
-            self.t += self.const_dt.dat.data[0]
+            self.t += self.qg_class.const_dt.dat.data[0]
+
+
+class two_level_quasi_geostrophic(object):
+
+    def __init__(self, dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, variance):
+
+        """ class specifying a correlated randomly forced quasi geostrophic model
+        on two levels
+
+            Arguments:
+
+            :arg dg_fs_c: The discontinuous :class:`FunctionSpace` used for Potential Vorticity
+                          on the coarse level
+            :type dg_fs_c: :class:`FunctionSpace`
+
+            :arg cg_fs_c: The continuous :class:`FunctionSpace` used for the Streamfunction
+                          on the coarse level
+            :type cg_fs_c: :class:`FunctionSpace`
+
+            :arg dg_fs_f: The discontinuous :class:`FunctionSpace` used for Potential Vorticity
+                          on the fine level
+            :type dg_fs_f: :class:`FunctionSpace`
+
+            :arg cg_fs_f: The continuous :class:`FunctionSpace` used for the Streamfunction
+                          on the fine level
+            :type cg_fs_f: :class:`FunctionSpace`
+
+            :arg variance: Variance of correlated random forcing
+            :type variance: int, if zero, forcing is off
+
+        """
+
+        self.qg_class_c = base_class(dg_fs_c, cg_fs_c, variance)
+        self.qg_class_f = base_class(dg_fs_f, cg_fs_f, variance)
+
+        self.variance = variance
+        self.mesh_c = self.qg_class_c.mesh
+        self.mesh_f = self.qg_class_f.mesh
+
+        # check for hierarchy existance
+        hierarchy_c, self.lvl_c = get_level(self.mesh_c)
+        hierarchy_f, self.lvl_f = get_level(self.mesh_f)
+        if hierarchy_c is not hierarchy_f:
+            raise ValueError('Coarse and fine meshes need to be on same hierarchy')
+        else:
+            self.mesh_hierarchy = hierarchy_c
+
+        # check levels
+        if self.lvl_c is not self.lvl_f - 1:
+            raise ValueError('Coarse level is not one below fine level')
+
+        self.dt_c = self.qg_class_c.dt
+        self.dt_f = self.qg_class_f.dt
+
+        # build a Function for aggregate u forcing from fine
+        self.aggregate_u = Function(self.qg_class_f.Vcg)
+
+        # check for refinement in time-steps
+        if self.mesh_hierarchy.refinements_per_level is not 1:
+            raise ValueError('Currently only 1 refinement per level can be used')
+
+        if np.abs(self.dt_c - (2 * self.dt_f)) > 1e-5:
+            raise ValueError('Time-steps are not of correct refinement')
+
+        self.q_ = tuple([self.qg_class_c.q_, self.qg_class_f.q_])
+        self.psi_ = tuple([self.qg_class_c.psi_, self.qg_class_f.psi_])
+
+        self.t = 0
+
+        # for checking
+        self.__t_c = 0
+        self.__t_f = 0
+
+        super(two_level_quasi_geostrophic, self).__init__()
+
+    def initial_condition(self, ufl_expression_c, ufl_expression_f):
+
+        # check for initial time
+        if self.t != 0:
+            raise ValueError("can't set initial condition when time is not zero")
+
+        self.qg_class_c.initial_condition(ufl_expression_c)
+        self.qg_class_f.initial_condition(ufl_expression_f)
+
+    def timestepper(self, T):
+
+        if self.t > T:
+            raise ValueError("can't timestep to t when t is less than current time")
+
+        # if the actual time, just return nothing
+        if self.t == T:
+            return
+
+        while self.t < T:
+
+            if self.t + self.dt_f > T:
+
+                # reassign both coarse and fine time-steps
+                self.qg_class_c.const_dt.assign(T - self.t)
+                self.qg_class_f.const_dt.assign(T - self.t)
+
+                # do one solve for each and update time
+
+                # update fine u
+                self.qg_class_f._base_class__update_u()
+
+                # inject onto coarse u
+                inject(self.qg_class_f.u_, self.qg_class_c.u_)
+
+                # update both forcing
+                self.qg_class_c._base_class__update_q_forced()
+                self.qg_class_f._base_class__update_q_forced()
+
+                # timestep
+                self.qg_class_c.timestep()
+                self.qg_class_f.timestep()
+
+                # update time
+                self.t += self.qg_class_c.const_dt.dat.data[0]
+
+                self.__t_c += self.qg_class_c.const_dt.dat.data[0]
+                self.__t_f += self.qg_class_f.const_dt.dat.data[0]
+                assert self.__t_c == self.t and self.__t_f == self.t
+
+            else:
+
+                self.qg_class_f.const_dt.assign(self.dt_f)
+
+                # solve one fine then one fine and coarse
+
+                # update fine u
+                self.qg_class_f._base_class__update_u()
+                self.aggregate_u.assign(0)
+                self.aggregate_u.assign(self.aggregate_u + self.qg_class_f.u_)
+
+                # update forcing and time-step
+                self.qg_class_f._base_class__update_q_forced()
+                self.qg_class_f.timestep()
+
+                self.__t_f += self.qg_class_f.const_dt.dat.data[0]
+
+                # now check if another whole fine timestep can be done
+                if self.t + self.dt_f + self.dt_f > T:
+
+                    # reassign both coarse and fine time-steps
+                    self.qg_class_c.const_dt.assign(T - self.t)
+                    self.qg_class_f.const_dt.assign(T - self.t - self.dt_f)
+
+                else:
+
+                    self.qg_class_c.const_dt.assign(self.dt_c)
+                    self.qg_class_f.const_dt.assign(self.dt_f)
+
+                # update fine u
+                self.qg_class_f._base_class__update_u()
+                self.aggregate_u.assign(self.aggregate_u + self.qg_class_f.u_)
+
+                # inject onto coarse u
+                inject(self.aggregate_u, self.qg_class_c.u_)
+
+                # update both forcing
+                self.qg_class_c._base_class__update_q_forced()
+                self.qg_class_f._base_class__update_q_forced()
+
+                # timestep
+                self.qg_class_c.timestep()
+                self.qg_class_f.timestep()
+
+                # update time
+                self.t += self.qg_class_c.const_dt.dat.data[0]
+
+                self.__t_c += self.qg_class_c.const_dt.dat.data[0]
+                self.__t_f += self.qg_class_f.const_dt.dat.data[0]
+                assert self.__t_c == self.t and self.__t_f == self.t
--- tests/test_qg.py
+++ tests/test_qg.py
@@ -7,6 +7,28 @@ import numpy as np
 from quasi_geostrophic_model import *
 
 
+def test_two_level_random_u_c():
+
+    mesh = UnitSquareMesh(2, 2)
+    mesh_h = MeshHierarchy(mesh, 1)
+    dg_fs_c = FunctionSpace(mesh_h[0], 'DG', 0)
+    cg_fs_c = FunctionSpace(mesh_h[0], 'CG', 1)
+    dg_fs_f = FunctionSpace(mesh_h[1], 'DG', 0)
+    cg_fs_f = FunctionSpace(mesh_h[1], 'CG', 1)
+
+    var = 2.0
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+
+    ts = np.linspace(1, 3, 3)
+    for i in range(len(ts)):
+        QG.timestepper(ts[i])
+        assert np.all(QG.qg_class_c.u_.dat.data != np.zeros(len(QG.qg_class_c.u_.dat.data)))
+        if i > 0:
+            assert np.all(QG.qg_class_c.u_.dat.data != u_)
+        u_ = np.copy(QG.qg_class_c.u_.dat.data)
+
+
 def test_zero_solution_both_psi_and_q():
 
     mesh = UnitSquareMesh(2, 2)
@@ -23,6 +45,28 @@ def test_zero_solution_both_psi_and_q():
     assert np.all(QG.psi_.dat.data[:] == 0.0)
 
 
+def test_zero_solution_both_psi_and_q_2():
+
+    mesh = UnitSquareMesh(2, 2)
+    mesh_h = MeshHierarchy(mesh, 1)
+    dg_fs_c = FunctionSpace(mesh_h[0], 'DG', 0)
+    cg_fs_c = FunctionSpace(mesh_h[0], 'CG', 1)
+    dg_fs_f = FunctionSpace(mesh_h[1], 'DG', 0)
+    cg_fs_f = FunctionSpace(mesh_h[1], 'CG', 1)
+
+    var = 0.0
+
+    # no initial condition
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.timestepper(1.0)
+
+    assert np.all(QG.q_[0].dat.data[:] == 0.0)
+    assert np.all(QG.psi_[0].dat.data[:] == 0.0)
+
+    assert np.all(QG.q_[1].dat.data[:] == 0.0)
+    assert np.all(QG.psi_[1].dat.data[:] == 0.0)
+
+
 def test_zero_solution_q():
 
     mesh = UnitSquareMesh(2, 2)
@@ -37,6 +81,28 @@ def test_zero_solution_q():
 
     assert np.all(QG.q_.dat.data[:] == 0.0)
     assert np.any(QG.psi_.dat.data[:] != 0.0)
+
+
+def test_zero_solution_q_2():
+
+    mesh = UnitSquareMesh(2, 2)
+    mesh_h = MeshHierarchy(mesh, 1)
+    dg_fs_c = FunctionSpace(mesh_h[0], 'DG', 0)
+    cg_fs_c = FunctionSpace(mesh_h[0], 'CG', 1)
+    dg_fs_f = FunctionSpace(mesh_h[1], 'DG', 0)
+    cg_fs_f = FunctionSpace(mesh_h[1], 'CG', 1)
+
+    var = 1.0
+
+    # no initial condition
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.timestepper(1.0)
+
+    assert np.all(QG.q_[0].dat.data[:] == 0.0)
+    assert np.any(QG.psi_[0].dat.data[:] != 0.0)
+
+    assert np.all(QG.q_[1].dat.data[:] == 0.0)
+    assert np.any(QG.psi_[1].dat.data[:] != 0.0)
 
 
 def test_correct_fs():
@@ -71,10 +137,12 @@ def test_forcing_var():
 
     sample_forcing = np.zeros(n)
     for i in range(n):
-        QG._quasi_geostrophic__update_forcing()
-        sample_forcing[i] = QG.dw.dat.data[0]
+        QG.qg_class._base_class__update_u()
+        QG.qg_class._base_class__update_forcing()
+        sample_forcing[i] = QG.qg_class.dw.dat.data[0]
 
-    assert np.abs((var * QG.const_dt.dat.data) - np.var(sample_forcing)) < 1e-2
+    assert np.abs((var * QG.qg_class.const_dt.dat.data) -
+                  np.var(sample_forcing)) < 1e-2
 
 
 def test_random_q():
@@ -103,6 +171,44 @@ def test_random_q():
     assert norm(assemble(f - g)) > 0
 
 
+def test_random_q_2():
+
+    mesh = UnitSquareMesh(2, 2)
+    mesh_h = MeshHierarchy(mesh, 1)
+    dg_fs_c = FunctionSpace(mesh_h[0], 'DG', 0)
+    cg_fs_c = FunctionSpace(mesh_h[0], 'CG', 1)
+    dg_fs_f = FunctionSpace(mesh_h[1], 'DG', 0)
+    cg_fs_f = FunctionSpace(mesh_h[1], 'CG', 1)
+
+    var = 1.0
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+
+    x_c = SpatialCoordinate(mesh_h[0])
+    ufl_expression_c = sin(2 * pi * x_c[0])
+    x_f = SpatialCoordinate(mesh_h[1])
+    ufl_expression_f = sin(2 * pi * x_f[0])
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.initial_condition(ufl_expression_c, ufl_expression_f)
+    QG.timestepper(1.0)
+
+    assert isinstance(QG.q_, tuple)
+
+    f_c = Function(dg_fs_c).assign(QG.q_[0])
+    f_f = Function(dg_fs_f).assign(QG.q_[1])
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.initial_condition(ufl_expression_c, ufl_expression_f)
+    QG.timestepper(1.0)
+
+    g_c = Function(dg_fs_c).project(QG.q_[0])
+    g_f = Function(dg_fs_f).project(QG.q_[1])
+
+    assert norm(assemble(f_c - g_c)) > 0
+    assert norm(assemble(f_f - g_f)) > 0
+
+
 def test_deterministic_q():
 
     mesh = UnitSquareMesh(2, 2)
@@ -127,6 +233,44 @@ def test_deterministic_q():
     g = Function(dg_fs).project(QG.q_)
 
     assert norm(assemble(f - g)) == 0
+
+
+def test_deterministic_q_2():
+
+    mesh = UnitSquareMesh(2, 2)
+    mesh_h = MeshHierarchy(mesh, 1)
+    dg_fs_c = FunctionSpace(mesh_h[0], 'DG', 0)
+    cg_fs_c = FunctionSpace(mesh_h[0], 'CG', 1)
+    dg_fs_f = FunctionSpace(mesh_h[1], 'DG', 0)
+    cg_fs_f = FunctionSpace(mesh_h[1], 'CG', 1)
+
+    var = 0.0
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+
+    x_c = SpatialCoordinate(mesh_h[0])
+    ufl_expression_c = sin(2 * pi * x_c[0])
+    x_f = SpatialCoordinate(mesh_h[1])
+    ufl_expression_f = sin(2 * pi * x_f[0])
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.initial_condition(ufl_expression_c, ufl_expression_f)
+    QG.timestepper(1.0)
+
+    assert isinstance(QG.q_, tuple)
+
+    f_c = Function(dg_fs_c).assign(QG.q_[0])
+    f_f = Function(dg_fs_f).assign(QG.q_[1])
+
+    QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
+    QG.initial_condition(ufl_expression_c, ufl_expression_f)
+    QG.timestepper(1.0)
+
+    g_c = Function(dg_fs_c).project(QG.q_[0])
+    g_f = Function(dg_fs_f).project(QG.q_[1])
+
+    assert norm(assemble(f_c - g_c)) == 0
+    assert norm(assemble(f_f - g_f)) == 0
 
 
 if __name__ == "__main__":
