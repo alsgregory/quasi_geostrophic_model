f2f5d7b7061cfe58eaf81633a3be8e5539c4009e
Alastair <alsgregory@aol.com>
2017-02-28 15:23:50 +0000

    reduced variance for new scheme

--- demos/demo_single_res_const_ic_random_forcing.py
+++ demos/demo_single_res_const_ic_random_forcing.py
@@ -31,7 +31,7 @@ ufl_expression = (exp(-(pow(x[0] - 0.25, 2) / (2 * pow(0.1, 2)) +
                         pow(x[1] - 0.3, 2) / (2 * pow(0.1, 2)))))
 
 # variance of random forcing
-var = 2.0
+var = 0.25
 
 # set-up QG class
 QG = quasi_geostrophic(dg_fs, cg_fs, var)
@@ -41,8 +41,10 @@ QG.initial_condition(ufl_expression)
 
 # timestep
 qFile = File("q.pvd")
+psiFile = File("psi.pvd")
 Ts = np.linspace(0, 49, 50)
 for i in range(len(Ts)):
     QG.timestepper(Ts[i])
     # write to file
     qFile.write(QG.q_)
+    psiFile.write(QG.psi_)
--- demos/demo_two_level_const_ic_random_forcing.py
+++ demos/demo_two_level_const_ic_random_forcing.py
@@ -50,7 +50,7 @@ ufl_expression_f = (exp(-(pow(x_f[0] - 0.25, 2) / (2 * pow(0.1, 2)) +
                           pow(x_f[1] - 0.3, 2) / (2 * pow(0.1, 2)))))
 
 # variance of random forcing
-var = 2.0
+var = 0.25
 
 # set-up two level QG class
 QG = two_level_quasi_geostrophic(dg_fs_c, cg_fs_c, dg_fs_f, cg_fs_f, var)
@@ -61,9 +61,13 @@ QG.initial_condition(ufl_expression_c, ufl_expression_f)
 # timestep
 qFile_c = File("q_c.pvd")
 qFile_f = File("q_f.pvd")
+psiFile_c = File("psi_c.pvd")
+psiFile_f = File("psi_f.pvd")
 Ts = np.linspace(0, 49, 50)
 for i in range(len(Ts)):
     QG.timestepper(Ts[i])
     # write to file
     qFile_c.write(QG.q_[0])
     qFile_f.write(QG.q_[1])
+    psiFile_c.write(QG.psi_[0])
+    psiFile_f.write(QG.psi_[1])
